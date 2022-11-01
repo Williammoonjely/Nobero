498e74005a2e66fcc129ce735d0d84eb191f058b
vineethkumarvm <vineethvinuvijayakumarkottukka@gmail.com>
2022-11-01 16:44:05 +0530

    updated products page

--- product/views.py
+++ product/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Category,Product
 
 # Create your views here.
 
 def categories(request):
-    return render(request,'product/categories.html')
+    product = Product.objects.all()
+    print(product)
+    return render(request,'product/categories.html',{'product':product})
 
 def product(request):
-    return render(request,'product/product.html')
+    product = Product.objects.all()
+    
+    return render(request,'product/product.html',{'product':product})
