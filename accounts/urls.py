eb2a9e7b182fd698c4119a4d4045fb2017d2138c
Sreyas <sreyas0000@gmail.com>
2022-11-01 13:26:06 +0530

    accounts completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('signin',views.signin, name = 'signin'),
     path('signup',views.signup,name = 'signup'),
+    # path('user',views.user, name = 'user'),
     path('signout',views.signout, name = 'signout'),
 ]
