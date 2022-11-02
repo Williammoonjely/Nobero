4359758c0cb64ab654acb1934456c25c277477e6
Sreyas <sreyas0000@gmail.com>
2022-11-02 11:10:21 +0530

    superuser edited

--- accounts/models.py
+++ accounts/models.py
@@ -24,15 +24,15 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-        def create_superuser(self, email, name, password):
-            """ Create a new superuser profile """
-            user = self.create_user(email,name, password)
-            user.is_superuser = True
-            user.is_staff = True
+    def create_superuser(self, email, name, password):
+        """ Create a new superuser profile """
+        user = self.create_user(email,name, password)
+        user.is_superuser = True
+        user.is_staff = True
 
-            user.save(using=self._db)
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
 
