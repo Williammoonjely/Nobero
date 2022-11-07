621a82a20a7cb15c9375d30b57922cdfb7b8fe64
vineethkumarvm <vineethvinuvijayakumarkottukka@gmail.com>
2022-11-07 10:52:05 +0530

    updated page

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories',views.categories, name = 'categories'),
-    path('product',views.product,name = 'product'),
+    
+    # path('categories',views.categories, name = 'cate'),
+    path('categories/<int:id>',views.categories, name = 'categories'),
+    path('product/<int:id>',views.product,name = 'product'),
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
 from .models import Category,Product
-
+from django.views.generic import ListView
 # Create your views here.
 
-def categories(request):
-    product = Product.objects.all()
-    print(product)
+class Detail_view(ListView):
+    model= Product
+    template_name='categories.html'
+def categories(request,id):
+    
+    category =Category.objects.get(id=id)   
+    product = Product.objects.filter(category= category)
+    print(product,'=====')
     return render(request,'product/categories.html',{'product':product})
 
-def product(request):
-    product = Product.objects.all()
+    # else: 
+
+
+    #     product = Product.objects.all()
+    #     return render(request,'product/categories.html',{'product':product})
+
+def product(request,id):
+    products = Product.objects.get(id=id)
+    print(product)
     
-    return render(request,'product/product.html',{'product':product})
+    return render(request,'product/product.html',{'products':products})
